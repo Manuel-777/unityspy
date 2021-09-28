b55f1b90999b916ecac0ed5a602aae632ec89d2d
Manwe <manuel.etchegaray7@gmail.com>
2021-09-28 10:38:42 -0300

    bundle in main dll again

---
+++ src/HackF5.UnitySpy/Spy.cs
@@ -0,0 +1,38 @@
+﻿namespace HackF5.UnitySpy
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class Spy
+    {
+        // private IAssemblyImage image;
+
+        [DllExport]
+        public static int GetPID(string processName)
+        {
+            var process = -1;
+            try
+            {
+                var proc = Process.GetProcessesByName(processName).FirstOrDefault();
+                process = proc.Id;
+            }
+            catch
+            {
+                //
+            }
+            return process;
+        }
+
+        [DllExport]
+        public static string GetUUID(int pid)
+        {
+            var image = AssemblyImageFactory.Create(pid);
+
+            var uuid = image?["PAPA"]["_instance"]["_accountClient"]["<AccountInformation>k__BackingField"]["AccountID"];
+
+            return uuid ?? "Not Found";
+        }
+
+    }
+}
